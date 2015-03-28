cf6da6f49a54629d3c48e857f6ebcf09ed3cbab0
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-03-28 19:32:30 +0100

    Add basic project.

---
+++ Knob/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Knob
+//
+//  Created by Jelle Vandenbeeck on 28/03/15.
+//  Copyright (c) 2015 Jelle Vandenbeeck. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
