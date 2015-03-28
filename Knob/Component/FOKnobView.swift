ea30cad506f355e633e3b2803e7a1d9c3bdafa42
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-03-28 19:35:20 +0100

    Add basic knob.

---
+++ Knob/Component/FOKnobView.swift
@@ -0,0 +1,21 @@
+//
+//  FOKnobView.swift
+//  Knob
+//
+//  Created by Jelle Vandenbeeck on 28/03/15.
+//  Copyright (c) 2015 Jelle Vandenbeeck. All rights reserved.
+//
+
+import UIKit
+
+class FOKnobView: UIView {
+    
+    // MARK: - Init
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        backgroundColor = UIColor.blueColor()
+    }
+    
+}
